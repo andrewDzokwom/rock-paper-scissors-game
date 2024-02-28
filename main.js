0ed1d98e64b9817ca736bd5ea949629301e552a2
andrewDzokwom <andrewa441@gmail.com>
2024-02-28 19:57:56 +0100

    fixed minor bugs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let computerScore = document.querySelector('#c-score')
 let playerScore = document.querySelector('#p-score')
 let output  = document.querySelector('.result')
+let computer = document.querySelector('#computer-choice')
+let player = document.querySelector('#player-choice')
+
 // choice buttons 
 let rock = document.querySelector('.rock')
 let paper = document.querySelector('.paper')
@@ -10,10 +13,10 @@ let restart = document.querySelector('.reset')
 
 // listening to the choice, invoke play function
 rock.addEventListener('click',()=> play("rock"))
-paper.addEventListener('click', () =>play("paper"))
-scissors.addEventListener('click', () =>play("scissors"))
+paper.addEventListener('click', ()=> play("paper"))
+scissors.addEventListener('click', ()=> play("scissors"))
 
-
+let result = "";
 // create the function play
 function play(userChoice) {
     // Generate the computer's choice
@@ -21,10 +24,12 @@ function play(userChoice) {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
   
     // Determine the winner
-    let result;
+   
     if (userChoice === computerChoice) {
       result = "It's a tie!";
       console.log(userChoice)
+      computerScore.textContent++;
+      playerScore.textContent++;
       
     } else if (
       (userChoice === "rock" && computerChoice === "scissors") ||
@@ -40,12 +45,9 @@ function play(userChoice) {
       console.log(userChoice, computerChoice)
       computerScore.textContent++;
     }
-    output.textContent = result
+    // output.textContent = result
+    player.textContent = `Player choice: ${userChoice}`
+    computer.textContent = `Computer choice: ${computerChoice}`
+    //document.querySelector('.output>#output').innerText = 'test'
 }
 
-// reset button that is put all to zero
-restart.addEventListener('click', () =>{
-    playerScore.textContent = 0
-    computerScore.textContent = 0
-
-})
