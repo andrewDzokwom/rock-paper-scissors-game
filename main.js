f8eeb87d70e99e66e964e0e415152bf2fec7ad70
andrewDzokwom <andrewa441@gmail.com>
2024-11-05 15:23:36 +0100

    correct tie outcome : zero for both

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let computerScore = document.querySelector('#c-score')
 let playerScore = document.querySelector('#p-score')
-let output  = document.querySelector('.result')
+let output = document.querySelector('.result')
 let computer = document.querySelector('#computer-choice')
 let player = document.querySelector('#player-choice')
 let winner = document.querySelector('#winner')
@@ -14,12 +14,12 @@ let restart = document.querySelector('#reset')
 
 
 // listening to the choice, invoke play function
-rock.addEventListener('click',()=> play("rock"))
-paper.addEventListener('click', ()=> play("paper"))
-scissors.addEventListener('click', ()=> play("scissors"))
+rock.addEventListener('click', () => play("rock"))
+paper.addEventListener('click', () => play("paper"))
+scissors.addEventListener('click', () => play("scissors"))
 
 // reset game with event listener
-restart.addEventListener('click', ()=>{
+restart.addEventListener('click', () => {
   playerScore.textContent = 0
   computerScore.textContent = 0
   winner.textContent = ""
@@ -30,30 +30,30 @@ restart.addEventListener('click', ()=>{
 let result = "";
 // create the function play
 function play(userChoice) {
-    // Generate the computer's choice
-    let choices = ["rock", "paper", "scissors"];
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  
-    // Determine the winner
-   
-    if (userChoice === computerChoice) {
-      result = "It's a tie!";
-      // computerScore.textContent++;
-      // playerScore.textContent++;
-      
-    } else if (
-      (userChoice === "rock" && computerChoice === "scissors") ||
-      (userChoice === "paper" && computerChoice === "rock") ||
-      (userChoice === "scissors" && computerChoice === "paper")
-    ) {
-      result = "You win!";
-      playerScore.textContent++;
-    } else {
-      result = "You lose!";
-      computerScore.textContent++;
-    }
-    player.textContent = `Player choice: ${userChoice}`
-    computer.textContent = `Computer choice: ${computerChoice}`
-    winner.textContent = result
+  // Generate the computer's choice
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  // Determine the winner
+
+  if (userChoice === computerChoice) {
+    result = "It's a tie!";
+    // computerScore.textContent++;
+    // playerScore.textContent++;
+
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = "You win!";
+    playerScore.textContent++;
+  } else {
+    result = "You lose!";
+    computerScore.textContent++;
+  }
+  player.textContent = `Player choice: ${userChoice}`
+  computer.textContent = `Computer choice: ${computerChoice}`
+  winner.textContent = result
 }
 
